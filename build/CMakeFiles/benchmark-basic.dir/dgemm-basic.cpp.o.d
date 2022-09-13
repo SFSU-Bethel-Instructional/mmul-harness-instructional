ed4a18d9992f36965aa28fc80d67dbb0d19d95df
Elliot Kang <kkang2097@gmail.com>
2022-09-12 23:45:20 -0700

    back to normal

--- build/CMakeFiles/benchmark-basic.dir/dgemm-basic.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-basic.dir/dgemm-basic.cpp.o: \
- /home/elliot/Documents/GitHub/mmul-harness-instructional/dgemm-basic.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/benchmark-blocked.dir/dgemm-blocked.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/benchmark-blocked.dir/dgemm-blocked.cpp.o: \
- /home/elliot/Documents/GitHub/mmul-harness-instructional/dgemm-blocked.cpp \
- /usr/include/stdc-predef.h
