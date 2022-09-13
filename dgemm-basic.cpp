97e26280d8f36952f38d8ba3c05cd8815cc5c74c
Elliot Kang <kkang2097@gmail.com>
2022-09-12 22:41:41 -0700

    Update dgemm-basic.cpp

--- dgemm-basic.cpp
+++ dgemm-basic.cpp
@@ -11,14 +11,14 @@ void square_dgemm(int n, double* A, double* B, double* C)
    //Then add our triple for-loop
    for (int i = 0; i < n; i++){
      for(int j = 0; j < n; j++){
-       C[j*n + i] = 0;
+       //C[i*n + j] = 0;
        for(int k = 0; k < n; k++){
          //
         //Row major order:
         //prod[i][j] += A[i][k] + B[k][j]
         //Column major order:
         //prod[i][j] += A[k][j] + B[i][k]
-        C[j*n + i] = C[j*n + i] + A[k*n + i] * B[j*n + k];
+        C[i*n + j] = C[i*n + j] + A[k*n + j] * B[i*n + k];
        }
      }
    }
